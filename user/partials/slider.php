06d889bad4bab764c23b10c5b7aa38c9e06e2e11
ldthanh0301 <ldthanh0301@gmail.com>
2023-04-26 19:28:35 +0700

    first commit

--- user/partials/slider.php
+++ user/partials/slider.php
@@ -3,13 +3,13 @@
                         <div class="slider-item col-md-6 col-sm">
                             <div class="slider-card">
                                 <div class="slider-card__title">Mở bán hàng tuần</div>
-                                <img class="slider-card__img" src='./assets/img/banner/banner.jpg'/>
+                                <img class="slider-card__img" src='./assets/img/slider/slider1.jpg'/>
                             </div>
                         </div>
                         <div class="slider-item col-md-6 col-sm">
                             <div class="slider-card">
                                 <div class="slider-card__title">Mở bán hàng tuần</div>
-                                <img class="slider-card__img" src='./assets/img/banner/banner3.jpg'/>
+                                <img class="slider-card__img" src='./assets/img/slider/slider2.jpg'/>
                             </div>
                         </div>
                     </div>
